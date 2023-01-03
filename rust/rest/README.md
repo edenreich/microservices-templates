e493ea123f290ba05274f3238254b1732ac2304f
Eden <eden.reich@gmail.com>
2023-01-03 02:56:04 +0100

    Package the service
    Also add missing process signals

---
+++ rust/rest/README.md
@@ -0,0 +1,25 @@
+## Rust Restful service
+
+This is a simple example of a restful service written in Rust.
+
+### Prerequisites
+
+- [Docker](https://www.docker.com/products/docker-desktop/)
+
+### Build
+
+To build the service, run:
+
+    $ make build
+
+### Run
+
+To run the service:
+
+    $ make run
+
+### Test
+
+To test the service:
+
+    $ make test
--- rust/rest/src/main.rs
+++ rust/rest/src/main.rs
@@ -10,6 +10,20 @@ use warp::Filter;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ctrl_c = tokio::signal::ctrl_c();
+    tokio::spawn(async move {
+        ctrl_c.await.unwrap();
+        println!("Ctrl+C received, shutting down");
+        std::process::exit(0);
+    });
+
+    let mut sigterm = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())?;
+    tokio::spawn(async move {
+        sigterm.recv().await;
+        println!("SIGTERM received, shutting down");
+        std::process::exit(0);
+    });
+
     let db = Arc::new(Mutex::new(Database::new()));
     let payments_endpoints = payments_filter(db.clone());
 
